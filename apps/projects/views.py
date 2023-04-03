17166fcb45bf543ea27dec915226f4f164171182
qsmsoft <qsmsoft@gmail.com>
2023-04-03 14:48:31 +0500

    Authentication

--- apps/projects/views.py
+++ apps/projects/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Project
 from .forms import ProjectForm
+
 # Create your views here.
 
 
@@ -16,6 +18,7 @@ def project(request, pk):
     return render(request, 'projects/single-project.html', {'project': project_obj, 'tags': tags})
 
 
+@login_required(login_url="login")
 def create_project(request):
     form = ProjectForm()
 
@@ -29,6 +32,7 @@ def create_project(request):
     return render(request, "projects/project_form.html", context)
 
 
+@login_required(login_url="login")
 def update_project(request, pk):
     project = Project.objects.get(id=pk)
     form = ProjectForm(instance=project)
@@ -43,6 +47,7 @@ def update_project(request, pk):
     return render(request, "projects/project_form.html", context)
 
 
+@login_required(login_url="login")
 def delete_project(request, pk):
     project = Project.objects.get(id=pk)
     if request.method == 'POST':
--- apps/users/models.py
+++ apps/users/models.py
@@ -10,7 +10,7 @@ class Profile(models.Model):
         User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, blank=True, null=True)
     email = models.EmailField(max_length=500, blank=True, null=True)
-    user_name = models.CharField(max_length=200, blank=True, null=True)
+    username = models.CharField(max_length=200, blank=True, null=True)
     location = models.CharField(max_length=200, blank=True, null=True)
     short_intro = models.CharField(max_length=200, blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
