00af425db25f98faf4f77858bc0e1b1111b5dcbc
qsmsoft <qsmsoft@gmail.com>
2023-04-07 01:34:59 +0500

    JWT updated

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,16 @@ from django.urls import path
 from . import views
 
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
 urlpatterns = [
+    path('users/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     path('', views.get_routes),
     path('projects/', views.get_projects),
     path('projects/<str:pk>/', views.get_project),
--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from .serializers import ProjectSerializer
 from apps.projects.models import Project
@@ -20,6 +21,7 @@ def get_routes(request):
 
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def get_projects(request):
     projects = Project.objects.all()
     serializer = ProjectSerializer(projects, many=True)
